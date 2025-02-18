2a3e3c8f685efbf57ece01b2a2cde1b3fe54a6d3
Andres Agudelo <andresagudelo1006@gmail.com>
2025-02-18 14:36:41 -0500

    Refactor payment notification route: remove commented code and update logging

--- src/app/api/paymentNotification/route.ts
+++ src/app/api/paymentNotification/route.ts
@@ -3,22 +3,20 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { MercadoPagoConfig, Payment } from 'mercadopago'
 
+// config mercadopago access token and clave secreta
+/* const config = new MercadoPagoConfig({
+  accessToken:
+    process.env.MERCADO_PAGO_ACCESS_TOKEN ||
+    'APP_USR-7917833599682123-121021-f43531d4b7864edde3be7beb8ed22a77-122889697',
+  clientSecret:
+    process.env.MERCADO_PAGO_CLIENT_SECRET || 'd5e339984067e2bfb33c8ed801ed72418173589e562e837a603166f1321085a0'
+}) */
+
 export async function POST(request: NextRequest) {
-  const { id, data } = await request.json()
+  //get body from request
+  const body = await request.json()
 
-  console.log('id', id)
-  console.log('fata', data)
-
-  // send message whatsapp to freddo
-  const message = `
-  Hola,
-  El pago de ${data.transaction_amount} se ha realizado correctamente.
-  `
-
-  // send message whatsapp to freddo
-  const response = await fetch('https://api.whatsapp.com/send?phone=573205389740&text=${message}')
-
-  console.log('response', response)
+  console.log('body', body)
 
   return NextResponse.json({ message: 'Payment notification received' })
 }
